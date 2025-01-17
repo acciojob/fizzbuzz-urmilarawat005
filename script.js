08d434c5339b350dfebd54643d2d89581b0b8fc6
urmilarawat005 <u4urmilarawat@gmail.com>
2025-01-17 21:05:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+for (let i = 1; i <= 20; i++) {
+    let output = '';
+    
+    if (i % 3 === 0) {
+        output += 'Fizz';
+    }
+    if (i % 5 === 0) {
+        output += 'Buzz';
+    }
+    
+    if (output === '') {
+        output = i.toString();
+    }
+    
+    alert(output);
+}
